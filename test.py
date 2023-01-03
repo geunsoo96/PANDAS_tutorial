3db7484b5e7021822c8c0f27912b01b8b43f7fc3
KinGeunSoo <geunsoo1106@gmail.com>
2023-01-03 12:10:13 +0900

    pandas 딕셔너리에서 데이터프레임 생성해보기

--- test.py
+++ test.py
@@ -10,4 +10,12 @@ data = {
 df = pd.DataFrame(data, index=data['year'])
 # 인덱스 추가 가능
 print(df)
+#       year  GDP rate     GDP
+# 2016  2016       2.8  1.637M
+# 2017  2017       3.1   1.73M
+# 2018  2018       3.0   1.83M
 print("row labels:", df.index)
+# row labels: Int64Index([2016, 2017, 2018], dtype='int64')
+print("column labels:", df.columns)
+# column labels: Index(['year', 'GDP rate', 'GDP'], dtype='object')
+print("head:", df.head())
