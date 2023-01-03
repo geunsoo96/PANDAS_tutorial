51062317c416fc9c7d5182ec142cf323e0e2d4f1
geunsoo <geunsoo1106@gmail.com>
2023-01-03 17:41:44 +0900

    판다스 유저 기초 학습 추가

--- python/users.py
+++ python/users.py
@@ -19,3 +19,43 @@ print(dfUsers)
 # 컬럼 설정해보기
 dfUsers.columns = ['name', 'height', 'age']
 print(dfUsers)
+#     name  height  age
+# 0   철수     180    5
+# 1   민수     175    5
+# 2   짱구     130    5
+
+# 딕셔너리 전달
+users = {
+    'name':['철수', '민수', '짱구'],
+    'height': [180, 175, 130],
+    'age': [25, 30, 5]
+}
+users
+# {'name': ['철수', '민수', '짱구'], 'height': [180, 175, 130], 'age': [25, 30, 5]}
+
+dfUsers = pd.DataFrame(users)
+dfUsers
+#     name  height  age
+# 0   철수     180   25
+# 1   민수     175   30
+# 2   짱구     130    5
+
+# 리스트 in 딕셔너리 전달
+# 실제 현업에서는 api 요청의 결과로 이런 형식의 데이터 포멧으로 올 가능성이 크다.
+users = [
+    {'name': '철수', 'height': 180, 'age': 25},
+    {'name': '민수', 'height': 170, 'age': 30},
+    {'name': '짱구', 'height': 130, 'age': 5}
+]
+users
+# [{'name': '철수', 'height': 180, 'age': 25},
+#  {'name': '민수', 'height': 170, 'age': 30},
+#  {'name': '짱구', 'height': 130, 'age': 5}]
+
+dfUsers = pd.DataFrame(users)
+dfUsers
+#     name  height  age
+# 0   철수     180   25
+# 1   민수     175   30
+# 2   짱구     130    5
+
